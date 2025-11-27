37eaff5997043585f34159c11bb28ce5755d7665
Hammad Ali <hammadali78056@gmail.com>
2025-11-27 12:22:51 +0500

    Initial Commit

---
+++ TikTok-Clone-Project/App/TikTok_Clone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_Clone_ProjectApp.swift
+//  TikTok-Clone-Project
+//
+//  Created by Hammad Ali on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_Clone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TikTok-Clone-Project/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTok-Clone-Project
+//
+//  Created by Hammad Ali on 27/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
